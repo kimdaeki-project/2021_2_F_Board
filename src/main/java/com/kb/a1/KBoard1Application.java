3231c16f967f0436d5a25a512725c9cfb2b54700
kakapo9090 <powercome93@gmail.com>
2021-11-15 10:10:22 +0900

    Initial commit

---
+++ src/main/java/com/kb/a1/KBoard1Application.java
@@ -0,0 +1,13 @@
+package com.kb.a1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KBoard1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KBoard1Application.class, args);
+	}
+
+}
